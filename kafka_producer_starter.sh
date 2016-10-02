bb741acbfd8a6586eaf6bad41b8ce76c6c44f793
Ubuntu <ubuntu@ip-172-30-0-54.ec2.internal>
2016-10-02 09:46:02 +0000

    Add current directory to CLASSPATH

--- kafka_producer_starter.sh
+++ kafka_producer_starter.sh
@@ -4,7 +4,7 @@ i=1
 while read line
 do
   echo "# Starting Kafka producer ($i) with throughput = $line "
-  java -cp ./lib/* setup.core -r -t $line &
+  java -cp .:./lib/* setup.core -r -t $line &
   i=$((i+1))
 done < "${1:-/dev/stdin}"
 
